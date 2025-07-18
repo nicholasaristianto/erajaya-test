ab411e5d206e2f294c1dcfafb96d7698fa809745
nicholasaristianto <165622432+nicholasaristianto@users.noreply.github.com>
2025-07-18 23:28:57 +0700

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('/dashboard/data', [DashboardController::class, 'getData'])->name('dashboard.data');
